1d6d03558d3e9402f7981132f7e9719e734d35b9
herom <jerome.manset22@gmail.com>
2023-02-20 17:55:50 +0800

    add functionality to the app using React useState and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 const App = () => {
+
+  const [advice, setAdvice] = useState([])
+
+  async function getAdvice(){
+    const response = await fetch('	https://api.adviceslip.com/advice');
+    const result = await response.json()
+    setAdvice(result.slip)
+  }
+
+  useEffect(() => {
+    getAdvice()
+  },[])
+
+
+  function handleClick(){
+   setAdvice(getAdvice())
+  }
+
   return (
     <div className='app--container'>
       <div className='quote--container'>
-        <p className='quote--id'>Advice #117</p>
-        <p className='quote--text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, nesciunt.</p>
+        <p className='quote--id'>Advice # {advice.id}</p>
+        <p className='quote--text'>"{advice.advice}"</p>
         <div className='quote--divider'></div>
-        <button>
-          <img src="/src/assets/images/icon-dice.svg" alt="" />
+        <button onClick={handleClick}>
+          <img src="/src/assets/images/icon-dice.svg" alt="dice" />
         </button>
       </div>
     </div>
