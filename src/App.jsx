d11e7beced366e1277252f07863c171ea5807f57
herom <jerome.manset22@gmail.com>
2023-02-20 16:33:48 +0800

    add elements and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,15 @@ import './App.css'
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className='app--container'>
+      <div className='quote--container'>
+        <p className='quote--id'>Advice #117</p>
+        <p className='quote--text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, nesciunt.</p>
+        <div className='quote--divider'></div>
+        <button>
+          <img src="/src/assets/images/icon-dice.svg" alt="" />
+        </button>
+      </div>
     </div>
   )
 }
