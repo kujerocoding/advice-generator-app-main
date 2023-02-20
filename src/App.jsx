dbf2e5635af9d8c1391751340d56064cc69fffa5
herom <jerome.manset22@gmail.com>
2023-02-20 18:28:49 +0800

    clean and sort the code

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,12 @@ const App = () => {
     getAdvice()
   },[])
 
-
   function handleClick(){
    setAdvice(getAdvice())
   }
 
   return (
     <div className='app--container'>
-      <div>
       <div className='quote--container'>
         <p className='quote--id'>Advice # {advice.id}</p>
         <p className='quote--text'>"{advice.advice}"</p>
@@ -34,8 +32,6 @@ const App = () => {
       <div className='attribution'>
             Challenge by <a href="https://www.frontendmentor.io/challenges" target="_blank">Frontend Mentor.</a> Coded by <a href='https://github.com/kujerocoding' target="_blank">kujerocoding</a>
       </div>
-      </div>
-      
     </div>
   )
 }
